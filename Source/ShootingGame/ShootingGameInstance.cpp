0d677c1bcb615e8ae897faaf6ca5224e7e4e9e75
KumoriLee <ear1223881481@gmail.com>
2026-05-27 15:19:29 +0900

    Add LevelManagerSubsystem and integrate it
    Introduce ULevelManagerSubsystem (GameInstanceSubsystem) to centralize level list/config and transitions. Provides BlueprintCallable APIs (OpenLevel, OpenLevelByIndex, OpenNextLevel, OpenPreviousLevel, RestartCurrentLevel, RestartGame, SetLevelNames) and broadcasts FOnLevelTransition before calling UGameplayStatics::OpenLevel. LevelNames is configurable (Config, with sensible defaults) and CurrentLevelIndex is tracked. Update UShootingGameInstance to delegate level operations to the new subsystem. Also apply minor comment/encoding normalization to HealthComponent files.

--- Source/ShootingGame/ShootingGameInstance.cpp
+++ Source/ShootingGame/ShootingGameInstance.cpp
@@ -3,24 +3,34 @@
 
 #include "ShootingGameInstance.h"
 
-#include "Kismet/GameplayStatics.h"
-
 void UShootingGameInstance::Changelevel(int32 Index)
 {
-
+	if (ULevelManagerSubsystem* LM = GetSubsystem<ULevelManagerSubsystem>())
+	{
+		LM->OpenLevelByIndex(Index - 1);
+	}
 }
 
 void UShootingGameInstance::LoadNextLevel()
 {
-
+	if (ULevelManagerSubsystem* LM = GetSubsystem<ULevelManagerSubsystem>())
+	{
+		LM->OpenNextLevel();
+	}
 }
 
 void UShootingGameInstance::RestartCurrentLevel()
 {
-
+	if (ULevelManagerSubsystem* LM = GetSubsystem<ULevelManagerSubsystem>())
+	{
+		LM->RestartCurrentLevel();
+	}
 }
 
 void UShootingGameInstance::RestartGame()
 {
-
+	if (ULevelManagerSubsystem* LM = GetSubsystem<ULevelManagerSubsystem>())
+	{
+		LM->RestartGame();
+	}
 }
--- Source/ShootingGame/ShootingGameInstance.h
+++ Source/ShootingGame/ShootingGameInstance.h
@@ -1,9 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "LevelManagerSubsystem.h"
 #include "ShootingGameInstance.generated.h"
 
 /**
