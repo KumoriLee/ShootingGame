3e33e2a277a558d2cc57b4920cea6bc40000effa
KumoriLee <ear1223881481@gmail.com>
2026-05-29 02:26:38 +0900

    Add ScreenMessage widget bindings and setters
    Expose UTextBlock widgets in UScreenMessage and implement setters to update their text from C++.
    
    - Header: include TextBlock, add BindWidget UTextBlock* members (MessageTextBlock, PointTextBlock, TimeTextBlock) and declare SetMessageText/SetPointText/SetTimeText.
    - Source: implement SetMessageText (FString -> FText), SetPointText and SetTimeText (int32 -> FText) to update the bound text blocks.
    - Blueprint asset (WBP_ScreenMessage.uasset) updated to reflect the widget changes.
    
    These changes allow the game code to update on-screen messages, score and time displays from C++.

--- Source/ShootingGame/ScreenMessage.h
+++ Source/ShootingGame/ScreenMessage.h
@@ -1,9 +1,12 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+
+#include "Components/TextBlock.h"		// TextBlock 控件：显示静态文字
+
 #include "ScreenMessage.generated.h"
 
 /**
@@ -13,5 +16,27 @@ UCLASS()
 class SHOOTINGGAME_API UScreenMessage : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	/** 绑定到蓝图中的 MessageTextBlock 控件，用于显示倒计时/胜负文字 */
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UTextBlock* MessageTextBlock;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UTextBlock* PointTextBlock;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UTextBlock* TimeTextBlock;
+
+	/**
+	 * 设置屏幕显示文字
+	 * @param Message 要显示的字符串
+	 */
+	void SetMessageText(FString Message);
+
+	void SetPointText(int32 Point);
+
+	void SetTimeText(int32 Time);
+
 	
 };
